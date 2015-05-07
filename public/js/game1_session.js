d03011c45d71a00cdf92ff99e5105433b9df724c
ken <ken@me.com>
2015-05-07 09:57:05 +0800

    auto

--- public/js/game1_session.js
+++ public/js/game1_session.js
@@ -103,7 +103,7 @@ function startGame_ScreenSide() {
     setTimeout(showResult, 6000);
     console.log("Start game");
 	var list = document.getElementById("top")
-	list.removeChild(list.childNodes[0]);
+	document.removeChild(list);
 	set_system_settings_level();
 }
 
