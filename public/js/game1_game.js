b2de0530fca47111a78f0a71e999c71b66233076
ken <ken@me.com>
2015-05-07 13:25:40 +0800

    auto

--- public/js/game1_game.js
+++ public/js/game1_game.js
@@ -51,7 +51,7 @@ function releaseBall(){
 	var fx = (fx1-fx0)*2000;
 	var fy = (fy1-fy0)*-12000;
 	sendThrowMotion(fx,fy);
-	alert('fx:'+fx+' fy:'+fy);
+	//alert('fx:'+fx+' fy:'+fy);
 	ball_throwing = false
 }
 
